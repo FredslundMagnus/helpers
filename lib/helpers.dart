1b4a2cf62a3eaaab1055e076a77291605b9eb01a
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-07-29 12:24:46 +0200

    Fix

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -2,4 +2,5 @@ export 'package:painter/painter.dart';
 export 'package:printer/printer.dart';
 export 'package:extensions/extensions.dart';
 export 'package:flutter/material.dart';
+export 'package:flutter/foundation.dart';
 export 'dart:math';
